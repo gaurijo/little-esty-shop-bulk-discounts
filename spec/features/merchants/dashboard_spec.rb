d3399bb96bb6a6ebbb2dcba09dda10a197d78958
Gauri Joshi <gauri728@gmail.com>
2022-08-04 16:26:25 -0700

    fix routing errors for merchant discount path

--- spec/features/merchants/dashboard_spec.rb
+++ spec/features/merchants/dashboard_spec.rb
@@ -1,17 +1,5 @@
 require 'rails_helper'
 
-# Merchant Bulk Discounts Index
-
-# As a merchant
-# When I visit my merchant dashboard
-# Then I see a link to view all my discounts
-
-# When I click this link
-# Then I am taken to my bulk discounts index page
-# Where I see all of my bulk discounts including their
-# percentage discount and quantity thresholds
-# And each bulk discount listed includes a link to its show page
-
 RSpec.describe 'the merchant dashboard' do
   it 'shows a link to view all my discounts' do 
     merchant1 = Merchant.create!(name: 'Fake Merchant', status: 'Enabled')
